206ba3da02a7783e995ffc65b855a8db8ed485ba
Michal Radomski <michalra@op.pl>
2023-07-12 02:17:13 +0200

    Course - commit07-116

--- 03 Responsive HTML & CSS/09 OmniFood Effects/js/script.js
+++ 03 Responsive HTML & CSS/09 OmniFood Effects/js/script.js
@@ -50,7 +50,7 @@ const sectionHeroEl = document.querySelector(".section-hero");
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
-    console.log(ent);
+    console.log({ ent });
 
     if (ent.isIntersecting === false) {
       document.body.classList.add("sticky");
@@ -68,3 +68,26 @@ const obs = new IntersectionObserver(
   }
 );
 obs.observe(sectionHeroEl);
+
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  const flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  const isSupported = flex.scrollHeight === 1;
+  console.log({ isSupported });
+  flex.parentNode.removeChild(flex);
+
+  if (!isSupported) {
+    document.body.classList.add("no-flexbox-gap");
+  }
+}
+checkFlexGap();
+
+// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
